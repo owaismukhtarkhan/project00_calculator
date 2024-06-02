ccc0e083f3d92130d90ceb0d7860392432f7c7dc
Owais Mukhtar Khan <160421080+owaismukhtarkhan@users.noreply.github.com>
2024-06-02 13:19:03 +0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,25 +2,24 @@ var prompt = require("prompt-sync")();
 //
 // get input from the user.
 //
-let num1:number = prompt("Please insert first number:");
-let arth:string = prompt("Please insert desired function:");
-let num2:number = prompt("Please insert second number:");
+let num1: number = parseInt(prompt("Please insert first number:"));
+let arth: string = prompt("Please insert desired function (+, -, /, *):");
+let num2: number = parseInt(prompt("Please insert second number:"));
 
-if (arth == "+" ){
-    let add = num1 + num2
-    console.log(add)
-} 
-else if (arth == "-" ){
-    let minus = num1 - num2
-    console.log(minus)
-}
-else if (arth == "*" ){
-    let multiply = num1 * num2
-    console.log(multiply)
-}
-else if (arth == "/" ){
-    let divide = num1 / num2
-    console.log(divide)
+if (arth == "-") {
+  let minus = num1 - num2;
+  console.log(minus);
+} else if (arth === "+") {
+  let adds = num1 + num2;
+  console.log(adds);
+} else if (arth == "*") {
+  let multiply = num1 * num2;
+  console.log(multiply);
+} else if (arth == "/") {
+  let divide = num1 / num2;
+  console.log(divide);
+} else {
+  console.log("Please select the correct function (+, -, /, *)");
 }
 
 export {};
